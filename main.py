8530857bc135a18947c39be6840390576357c66a
azizomoussa65 <azizomoussa65@gmail.com>
2024-06-06 10:14:51 +0100

    add new committ

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World")
-p
+import sys
+from csv_utils import calculate_average
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <file_path> <column_name>")
+        return
+
+    file_path = sys.argv[1]
+    column_name = sys.argv[2]
+    calculate_average(file_path, column_name)
+
+if __name__ == "__main__":
+    main()
